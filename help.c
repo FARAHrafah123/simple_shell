5b836c2afc47153181e42c8309f0a444559a94b0
sala2022 <saloua.kasmibakkali@gmail.com>
2023-07-20 06:44:35 -0700

    update help

--- help.c
+++ help.c
@@ -1,5 +1,13 @@
 #include "shell.h"
 
+/**
+ * print_error_string -This function is used to print an error
+ *  message indicating that a particular command was not found.
+ *  @name:name of error
+ *  @command:command not found or caused the error.
+ *  @count_command:the line number where the error occurred
+ */
+
 void print_error_string(char *name, char *command, int count_command)
 {
 	print_error(name);
@@ -9,7 +17,13 @@ void print_error_string(char *name, char *command, int count_command)
 	print_error(command);
 	print_error(": ");
 	print_error("not found\n");
+
 }
+
+/**
+ * print_error - This function is responsible for printing an error message
+ * @str:containing the error message to be printed
+ */
 
 void print_error(char *str)
 {
@@ -19,6 +33,11 @@ void print_error(char *str)
 }
 
 
+/**
+ * free_comand_array - This fun  used to deallocate memory for an array of str
+ * @string:represents a dynamically allocated string
+ * @arr_tok: pointer to a pointer of characters
+ */
 
 void free_comand_array(char *string, char **arr_tok)
 {
